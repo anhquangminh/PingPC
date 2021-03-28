993016ffe053fcb849b1f3b3ea704d2a4651a600
t-bag <qminh97ictu@gmailcom>
2021-03-28 13:37:44 +0700

    Revert "Revert "ping pc""
    This reverts commit 56c25594d22ada6d1c7c69ce4aa004bee38b9a47.

---
+++ PingCommandReport - IP Range - Office-Line B Station - C Station/PingCommandReport/About.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PingCommandReport
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(29, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ping Command Resport";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(114, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Version 1.0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(278, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Copyright © 2019 Foxconn Funing IT All Rights Reserved";
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "About";
+            this.Text = "About";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ PingCommandReport - IP Range - Office-Line B Station - C Station/PingCommandReport/ExceptionIp.cs
@@ -0,0 +1,58 @@
+﻿using Ini;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingCommandReport
+{
+    public partial class ExceptionIp : Form
+    {
+        public ExceptionIp()
+        {
+            InitializeComponent();
+            getinforSave();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you want to change list ip exception this  Ip ?", "Change list ip ", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (result.Equals(DialogResult.OK))
+            {
+                saveinforOfProgram(richTextBox1.Text);
+                this.Close();
+            }
+        }
+
+
+        public void getinforSave()
+        {
+
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "\\save-ip-exception.ini"; //1
+
+            if (System.IO.File.Exists(filePath))
+            {
+                IniFile ini = new IniFile(filePath);
+                richTextBox1.Text = ini.IniReadValue("ip", "ipexception");
+            }
+            else
+            {
+
+            }
+        }
+
+        public void saveinforOfProgram(string richtext)
+        {
+
+            string fileLPath = AppDomain.CurrentDomain.BaseDirectory + "\\save-ip-exception.ini";
+            IniFile ini = new IniFile(fileLPath);
+            ini.IniWriteValue("ip", "ipexception", richtext);
+           
+        }
+    }
+}
